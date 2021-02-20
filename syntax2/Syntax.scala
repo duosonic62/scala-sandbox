f17031a1663a807c4bf3cc391ffd1cc12b478245
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-02-20 14:59:24 +0900

    add exam

--- syntax2/Syntax.scala
+++ syntax2/Syntax.scala
@@ -12,6 +12,17 @@ object Syntax {
 
     a.print()
     b.print()
+
+    val p = new Point3D(10, 20, 30)
+    println(p.x)
+    println(p.y)
+    println(p.z)
+
+    val rectangle: Shape = new Rectangle(10.0, 20.0)
+    println(rectangle.area)
+
+    val circle: Shape = new Circle(2.0)
+    println(circle.area)
   }
 }
 
@@ -28,5 +39,23 @@ class APrinter() {
 class BPrinter() extends APrinter {
   override def print(): Unit = {
     println("B")
+  }
+}
+
+class Point3D(val x: Int, val y: Int, val z: Int)
+
+abstract class Shape {
+  def area: Double
+}
+
+class Rectangle(val w: Double, val h: Double) extends Shape {
+  override def area: Double = {
+    w * h
+  }
+}
+
+class Circle(val r: Double) extends Shape {
+  override def area: Double = {
+    r * r * math.Pi
   }
 }
