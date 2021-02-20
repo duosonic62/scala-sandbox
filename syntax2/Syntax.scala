c362dab27de6ab58802ace26f9589ef05b3c6084
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2021-02-20 14:53:11 +0900

    add extends

--- syntax2/Syntax.scala
+++ syntax2/Syntax.scala
@@ -6,9 +6,27 @@ object Syntax {
 
     val fun = adder.add(2) _
     println(fun(3))
+
+    val a: APrinter = new APrinter()
+    val b: APrinter = new BPrinter()
+
+    a.print()
+    b.print()
   }
 }
 
 class Adder {
   def add(x: Int)(y: Int) = x + y
+}
+
+class APrinter() {
+  def print(): Unit = {
+    println("A")
+  }
+}
+
+class BPrinter() extends APrinter {
+  override def print(): Unit = {
+    println("B")
+  }
 }
